4410e642ddddbf7c58826983b7285a238ff0cd19
Victor Marcelino <victor.fmarcelino@gmail.com>
2019-08-24 00:02:23 -0300

    Added display_name to Place. Name is unique per user

--- server/resources/place_resources.py
+++ server/resources/place_resources.py
@@ -18,21 +18,30 @@ import db  # pylint: disable=relative-beyond-top-level
 class PlaceEndpoint(flask_restful.Resource):
     def post(self):  # create
         json = flask.request.json
-
-        are_fields_missing, missing_fields_message = helper_functions.are_fields_missing(json, 'name', 'token')
+        fields = ['name', 'display_name', 'token']
+        are_fields_missing, missing_fields_message = helper_functions.are_fields_missing(json, *fields)
         if are_fields_missing:
             return missing_fields_message, HTTPStatus.BAD_REQUEST
 
         name = json['name']
+        display_name = json['display_name']
         token = json['token']
 
         payload = jwt.decode(jwt=token, key=CONSTANTS.key, algorithms='HS256')
         administrator_id = payload['sub']
 
         admin_user_vertex = janusgraphy.Query.from_vertex_id(administrator_id).fetch_first()
-        place_vertex = db.Place(name=name, adm=admin_user_vertex)
-        place_vertex.add_to_graph()
-        return {"message": "Place created", 'id': place_vertex.graph_value.id}, HTTPStatus.OK
+
+        owned_places_query = admin_user_vertex.query().through_outgoing_edge(db.Administers)
+        owned_places_with_same_name = list(owned_places_query.filter_by_property(name=name).fetch_all())
+
+        if len(owned_places_with_same_name) == 0:
+            place_vertex = db.Place(name=name, display_name=display_name, adm=admin_user_vertex)
+            place_vertex.add_to_graph()
+            return {"message": "Place created", 'id': place_vertex.graph_value.id}, HTTPStatus.OK
+
+        else:
+            return "There is already a place with the same name", HTTPStatus.CONFLICT
 
     def put(self, administrator_id, place_id):  # update
         return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
