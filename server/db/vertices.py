2354b80b17838fccccd7d68719d8d9bb5467fd71
Victor Marcelino <victor.fmarcelino@gmail.com>
2019-08-27 17:00:43 -0300

    Added Place vertex structure

--- server/db/vertices.py
+++ server/db/vertices.py
@@ -8,6 +8,8 @@ class User(janusgraphy.Vertex):
 
 
 class Place(janusgraphy.Vertex):
+    Structure = ['name', 'display_name']
+
     def __init__(self, name: str, display_name: str, adm: User, **kwargs):
         super().__init__(name=name, display_name=display_name, **kwargs)
         adm.add_edge(Administers, self)
