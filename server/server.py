847f50c776a68844196ba2194f902cdcb9fac88c
Victor Marcelino <victor.fmarcelino@gmail.com>
2019-10-08 01:27:13 -0300

    Added CORS support for server

--- server/server.py
+++ server/server.py
@@ -1,11 +1,13 @@
 import flask
 import flask_restful
+from flask_cors import CORS
 
 # Local imports
 import db
 import resources
 
 app = flask.Flask(__name__)
+CORS(app)
 api = flask_restful.Api(app)
 
 db.connect()
@@ -17,6 +19,7 @@ api.add_resource(resources.PlaceEndpoint, '/place')  # create, get, update, dele
 api.add_resource(resources.ListPlacesEndpoint, '/place/list')  # list all places
 api.add_resource(resources.MenuListEndpoint, '/place/menu/list/<int:place_id>', '/menu/<int:menu_id>')
 api.add_resource(resources.MenuCreationEndpoint, '/menu')  # create, get, update, delete a menu
+
 
 #@api.representation('application/json')
 #def output_json(data, code, headers
